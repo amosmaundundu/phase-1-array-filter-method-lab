7fecfd8cbbf17805732e0dc8844839382015f6a2
amosmaundundu <amosmaundu5@gmail.com>
2022-08-23 12:05:12 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(drivers, string){
+    const match= drivers.filter((value)=>{
+
+      return value.toLowerCase()===string.toLowerCase();
+    });
+    return match;
+}
+function fuzzyMatch(drivers, string){
+ const filter = drivers.filter((value)=>{
+    return value.slice(0,2)==string.slice(0,2);
+ });
+ return filter;
+}
+function matchName(drivers,string){
+let match =drivers.filter((item)=>{
+    return item.name ===string
+})
+return match;
+ }
